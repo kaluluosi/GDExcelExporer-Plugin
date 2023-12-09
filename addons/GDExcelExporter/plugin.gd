15df30e9134c5e71b10cb863260cf31cd3b72480
kaluluosi111 <kaluluosi@gmail.com>
2023-12-09 21:30:56 +0800

    修复ee_path 变量被覆盖问题

--- addons/GDExcelExporter/plugin.gd
+++ addons/GDExcelExporter/plugin.gd
@@ -1,13 +1,13 @@
 tool
 extends EditorPlugin
 
-var settings_dir = {
+var settings_dir_property = {
 	"name": "GDExcelExporter/SettingsDir",
 	"type": TYPE_STRING,
 	"hint": PROPERTY_HINT_DIR,
 	"hint_string": "配置表所在目录"
 }
-var	ee_path = {
+var	ee_path_property = {
 		"name": "GDExcelExporter/cmd_path",
 		"type":TYPE_STRING,
 		"hint": PROPERTY_HINT_DIR,
@@ -15,15 +15,15 @@ var	ee_path = {
 	}
 
 func _enter_tree():
-	if not ProjectSettings.has_setting(settings_dir.name):
-		ProjectSettings.set_setting(settings_dir.name, "res://Settings")
-		ProjectSettings.add_property_info(settings_dir)
-		ProjectSettings.set_initial_value(settings_dir.name, "res://Settings")
+	if not ProjectSettings.has_setting(settings_dir_property.name):
+		ProjectSettings.set_setting(settings_dir_property.name, "res://Settings")
+		ProjectSettings.add_property_info(settings_dir_property)
+		ProjectSettings.set_initial_value(settings_dir_property.name, "res://Settings")
 	
-	if not ProjectSettings.has_setting(ee_path.name):
-		ProjectSettings.set_setting(ee_path.name, "ee")
-		ProjectSettings.add_property_info(ee_path)
-		ProjectSettings.set_initial_value(settings_dir.name, "ee")
+	if not ProjectSettings.has_setting(ee_path_property.name):
+		ProjectSettings.set_setting(ee_path_property.name, "ee")
+		ProjectSettings.add_property_info(ee_path_property)
+		ProjectSettings.set_initial_value(ee_path_property.name, "ee")
 	
 	add_tool_menu_item("ExcelExport", self, "gen_all")
 
@@ -32,20 +32,20 @@ func _exit_tree():
 
 func gen_all(ud):
 	
-	var settings = ProjectSettings[settings_dir.name]
-	var ee = ProjectSettings[ee_path.name]
+	var settings_path = ProjectSettings[settings_dir_property.name]
+	var ee_path = ProjectSettings[ee_path_property.name]
+	var settings_abs_path = ProjectSettings.globalize_path(settings_path)
 	
 	var dir = Directory.new()
-	if not dir.dir_exists(settings):
+	if not dir.dir_exists(settings_path):
 		var warning_box =AcceptDialog.new()
-		warning_box.dialog_text = settings + " 目录不存在！"
+		warning_box.dialog_text = settings_path + " 目录不存在！"
 		warning_box.window_title = "警告！GDExcelExporter"
 		get_editor_interface().get_editor_viewport().add_child(warning_box)
 		warning_box.popup_centered(Vector2(100,100))
 	else:
-		var abs_path = ProjectSettings.globalize_path(settings)
 		var warning_box =AcceptDialog.new()
-		warning_box.dialog_text = settings + "导出中..."
+		warning_box.dialog_text = settings_path + "导出中..."
 		warning_box.window_title = "GDExcelExporter"
 		get_editor_interface().get_editor_viewport().add_child(warning_box)
 		warning_box.popup_centered()
@@ -53,8 +53,8 @@ func gen_all(ud):
 
 		var output = []
 		print("=".repeat(10))
-		print("导出",abs_path,"下面的所有表")
-		OS.execute(ee,["gen-all","--cwd",abs_path],true,output)
+		print("导出",settings_abs_path,"下面的所有表")
+		OS.execute(ee_path,["gen-all","--cwd",settings_abs_path],true,output)
 		for line in output:
 			print(line)
 			
